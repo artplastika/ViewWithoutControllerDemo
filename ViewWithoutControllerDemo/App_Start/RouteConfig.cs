6008fbd7580f69fd0dc2912edc4c7ce412f8b02e
Victor Glushenkov <v.glushenkov@ts-soft.ru>
2014-10-30 15:32:02 +0300

    Try answer by haim770

---
+++ ViewWithoutControllerDemo/App_Start/RouteConfig.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace ViewWithoutControllerDemo
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{*viewPath}",
+                defaults: new { controller = "Default", action = "Get", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ ViewWithoutControllerDemo/Controllers/DefaultController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Mvc;
+
+namespace ViewWithoutControllerDemo.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public ActionResult Get(string viewPath)
+        {
+            var extractedPath = viewPath.Split('/');
+            this.ControllerContext.RouteData.Values["controller"] = extractedPath[0];
+            this.ControllerContext.RouteData.Values["action"] = extractedPath[1];
+            return View();
+        }
+    }
+}
